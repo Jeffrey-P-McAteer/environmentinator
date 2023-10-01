b47793cb48d2757c9b5b7abfe08e798147794eea
Jeffrey McAteer <jeffrey.p.mcateer@gmail.com>
2023-10-01 10:28:09 -0400

    v1 ensure_py_version

--- environmentinator/__init__.py
+++ environmentinator/__init__.py
@@ -10,6 +10,7 @@ import subprocess
 import importlib
 import inspect
 import shutil
+import time
 
 def ensure_module(module_name, package_name=None):
   '''
@@ -93,12 +94,86 @@ def delete_all_environmentinator_modules():
   if os.path.exists(env_target):
     shutil.rmtree(env_target)
 
-def ensure_py_version(comparison_chars, major_version, minor_version, addtl_runtime_locations=[]):
+def ensure_py_version(comparison_eval_str, addtl_runtime_locations=[], executable_name='python'):
     '''
-      TODO unimplemented!
+      This function checks the current runtime's major + minor version
     '''
-    raise Exception('TODO unimplemented!')
 
+    vers_major = sys.version_info[0]
+    vers_minor = sys.version_info[1]
+    vers_str = '{}.{}'.format(vers_major, vers_minor)
+
+    is_good_py_version = eval_expr('{}{}'.format(vers_str, comparison_eval_str))
+    if not is_good_py_version:
+  
+      if 't' in os.environ.get('ENVIRONMENTINATOR_IS_SUBPROCESS', '').lower():
+        raise Exception('Environmentinator is refusing to run ensure_py_version() from a sub-processes because you likely have an infinite loop in your dependency resolution!')
+
+      os.environ['PATH'] = os.pathsep.join( addtl_runtime_locations + os.environ.get('PATH', '').split(os.pathsep) )
+      os.environ['LD_LIBRARY_PATH'] = os.pathsep.join( addtl_runtime_locations + os.environ.get('LD_LIBRARY_PATH', '').split(os.pathsep) )
+
+      os.environ['ENVIRONMENTINATOR_IS_SUBPROCESS'] = 't'
+      proc = subprocess.Popen( [ executable_name ] + sys.argv )
+      while proc.poll() is None:
+        time.sleep(0.1)
+      child_ret_code = proc.poll()
+      sys.exit(child_ret_code)
+
+
+
+# Stolen from https://stackoverflow.com/a/9558001
+# to avoid eval()-ing strings
+
+import ast
+import operator as op
+
+# supported operators
+operators = {
+  ast.Add: op.add,
+  ast.Sub: op.sub,
+  ast.Mult: op.mul,
+  ast.Div: op.truediv,
+  ast.Pow: op.pow,
+  ast.BitXor: op.xor,
+  ast.USub: op.neg,
+  
+  ast.Not: op.not_,
+  ast.Eq: op.eq,
+  ast.NotEq: op.ne,
+  ast.Lt: op.lt,
+  ast.LtE: op.le,
+  ast.Gt: op.gt,
+  ast.GtE: op.ge,
+}
+
+def eval_expr(expr):
+    """
+    >>> eval_expr('2^6')
+    4
+    >>> eval_expr('2**6')
+    64
+    >>> eval_expr('1 + 2*3**(4^5) / (6 + -7)')
+    -5.0
+    """
+    print(f'expr={expr}')
+    return eval_(ast.parse(expr, mode='eval').body)
+
+def eval_(node):
+    if isinstance(node, ast.Num): # <number>
+        return node.n
+    elif isinstance(node, ast.BinOp): # <left> <operator> <right>
+        return operators[type(node.op)](eval_(node.left), eval_(node.right))
+    elif isinstance(node, ast.UnaryOp): # <operator> <operand> e.g., -1
+        return operators[type(node.op)](eval_(node.operand))
+    elif isinstance(node, ast.BoolOp): # <left> <operator> <right> e.g., True or False
+        return operators[type(node.op)](eval_(node.left), eval_(node.right))
+    elif isinstance(node, ast.Compare): # <left> <operator> <right> e.g., 1 > 2
+      if len(node.ops) == 1 and len(node.comparators) == 1:
+        return operators[type(node.ops[0])](eval_(node.left), eval_(node.comparators[0]))
+      else:
+        raise Exception('Multi-comparison shorthand not supported! Please replace eg (1 < x < 2) with (1 < x and x < 2)')
+    else:
+        raise TypeError(node)
 
 
 
