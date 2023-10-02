f6b07ec5d37cef62ac73634941feeac5ebd26659
Jeffrey McAteer <jeffrey.p.mcateer.civ@mail.mil>
2023-10-02 07:58:58 -0400

    Example 02, because windows doesn't have my Arch copy of python 3.10. Theory still stands, you just need to point to C:\Program Files\PythonA.B\bin

---
+++ examples/example_02.py
@@ -0,0 +1,13 @@
+import environmentinator
+
+json5 = environmentinator.ensure_module('json5')
+
+print(f'I have loaded json5 from {json5}')
+
+# We can specify addtl pip args as well, these are added after "python -m pip install --target={}"
+torch = environmentinator.ensure_module('torch', 'torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu117')
+
+print(f'I have loaded torch from {torch}')
+
+
+
